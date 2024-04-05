50ce143bda8d3ad2a063a6aa7d548d26c69ab787
DMYang9378 <dmyang@uds.ai>
2024-04-05 16:14:53 +0900

    Create Datasource Generator

--- grafana/public/app/features/test/testGenerator.tsx
+++ grafana/public/app/features/test/testGenerator.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios'
+import datasourceWindowPrometheus from './jsonFile/datasource-winodw-prometheus-test.json'
 
 export default function testGenerator() {
   const [formData, setFormData] = useState({ ip: '', servername: '', assetNum: '' })
-
+  // const selectTemplate = 'window'
   const handleInputChange = (event: { target: { name: string; value: string; }; }) => {
     const { name, value } = event.target;
     setFormData({
@@ -21,7 +22,12 @@ export default function testGenerator() {
         "title": formData.ip
       }
       const folderCreateResponse = await axios.post('http://localhost:3000/api/folders', folderReqData);
-      console.log(folderCreateResponse)
+      const newFolderUid = folderCreateResponse.data.uid
+      console.log(newFolderUid)
+      const newdataSourceData = { ...datasourceWindowPrometheus, name: `${formData.ip}-window`, url: `${formData.ip}:${datasourceWindowPrometheus.url}` }
+      console.log(newdataSourceData)
+      const DatasourceCreateResponse = await axios.post('http://localhost:3000/api/datasources/', newdataSourceData)
+      console.log(DatasourceCreateResponse)
     } catch (error) {
       console.error('Error createing folder', error);
     }
