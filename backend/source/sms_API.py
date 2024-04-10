ff1b2e33d54b30b29f5589dd9c58ab3979be62ad
DMYang9378 <dmyang@uds.ai>
2024-04-11 07:52:24 +0900

    send alert email backend logic

--- backend/source/sms_API.py
+++ backend/source/sms_API.py
@@ -1,24 +1,22 @@
-from flask import Blueprint, request
-import time
-import base64
-import hashlib
-import hmac
+from flask import Blueprint, request, render_template, current_app
+from flask_mail import Mail,Message
 
-sms_api = Blueprint('sms_api', __name__)
-
-# 문자 발송 로직
-def sendmsg(message):
-  return
+sms_api = Blueprint('sms_api', __name__, template_folder='../templates')
 
 # 루트 엔드포인트
-@sms_api.route('/send', methods=['POST'])
-def send_sms():
-    message = 'alert 아님'
+@sms_api.route('/send/email', methods=['POST'])
+def send_email():
+    mail=Mail(current_app)
     alert_data = request.json
     if alert_data['status'] == 'firing':
+        subject = alert_data['title']
         message = f"알람 제목은 {alert_data['title']} 발생 시간은 {alert_data['alerts'][0]['startsAt']}"
-        # sendmsg(message)
-    print(message)
-    return message
-
-
+        html_content = render_template('alert_template.html', alert_data=alert_data)
+        
+        recipients = ['dmyanguds@gmail.com']
+        msg = Message(subject=subject, recipients=recipients)
+        msg.html = html_content
+        mail.send(msg)
+        print(alert_data)
+        return 'Alarm 전송 성공'
+    return 'Alarm 아님'
--- backend/source/views.py
+++ backend/source/views.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+
 from main_API import main_api
 from alarmHistory_API import alarm_history_api
-# from sms_API import sms_api
+from sms_API import sms_api
 
 app = Flask(__name__)
 CORS(app)
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'dmyanguds@gmail.com'
+app.config['MAIL_PASSWORD'] =''
+app.config['MAIL_DEFAULT_SENDER'] = 'dmyanguds@gmail.com'
+
 # Blueprint 등록
 app.register_blueprint(main_api)
 app.register_blueprint(alarm_history_api)
-# app.register_blueprint(sms_api)
+app.register_blueprint(sms_api)
 
 if __name__ == '__main__':
     app.run(debug=True)
